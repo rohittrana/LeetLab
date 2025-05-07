eb794ea2fd819044200bf886e96abad1af5e3b57
rohittrana <rohitrana2429@gmail.com>
2025-05-07 18:28:07 +0530

    frontend folder addded

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,138 +1,134 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import {db} from '../libs/db.js'
-import jwt from 'jsonwebtoken';
-import { UserRole } from '../generated/prisma/index.js';
-export const register= async(req,res)=>{
-  const {email,name,password} =req.body;
-  try{
-      const existingUser = await db.user.findUnique({
-               where:{
-                              email
-               }
-      })
-      if(existingUser){
-               return res.status(400).json({
-                              error:"User is already exists"
-               })
-      }
-      const hashedPassword = await bcrypt.hash(password,10);
-      const newUser = await db.user.create({
-               data:{
-                              email,
-                              password:hashedPassword,
-                              name,
-                              role:UserRole.USER
-               }
-      })
-     const token = jwt.sign({id:newUser.id},process.env.JWT_SECRET,{
-               expiresIn:"7d"
-     })
+import express from "express";
+import bcrypt from "bcryptjs";
+import { db } from "../libs/db.js";
+import jwt from "jsonwebtoken";
+import { UserRole } from "../generated/prisma/index.js";
+export const register = async (req, res) => {
+  const { email, name, password } = req.body;
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      return res.status(400).json({
+        error: "User is already exists",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: UserRole.USER,
+      },
+    });
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
-     res.cookie('jwt',token,{
-         httpOnly:true,
-         sameSite:"strict",
-         secure:process.env.NODE_ENv !=="development",
-         maxAge:1000*60*60*24*7
-     })
-       res.status(201).json({
-        success:true,
-               message:"User created succesfully",
-                              user:{
-                                             id:newUser.id,
-                                             email:newUser.email,
-                                             name:newUser.name,
-                                             role:newUser.role,
-                                             image:newUser.image
-                              }
-               
-       })
-
-  }catch(error){
-           console.log("error creating user: ",error);
-           res.status(500).json({
-               error:"Error creating user"
-           })
-  }
-}
-export const login = async(req,res)=>{
-  const {email,password}= req.body;
-  try{
-       const user = await db.user.findUnique({
-        where:{
-          email
-        }
-       })
-       if(!user){
-        return res.status(401).json({
-           error:"User is not found"
-        })
-      }
-      const isMatch = await bcrypt.compare(password,user.password);
-      if(!isMatch){
-        return res.status(401).json({
-          erro:"Invalid credentials"
-        })
-      }
-      const token = jwt.sign({id:user.id},process.env.JWT_SECRET,{
-        expiresIn:"7d"
-      })
-      res.cookie("jwt",token,{
-        httpOnly:true,
-        sameSite:"strict",
-
-        secure:process.env.NODE_ENV !== "development",
-        maxAge:1000*60*60*24*7
-      })
-      res.status(200).json({
-        success:true,
-        message:"User logged in successfully",
-        user:{
-          id:user.id,
-          email:user.email,
-          name:user.name,
-          role:user.role,
-          image:user.image
-        }
-      })
-  }catch(error){
-    console.log('Error Sign user:',error);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENv !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    res.status(201).json({
+      success: true,
+      message: "User created succesfully",
+      user: {
+        id: newUser.id,
+        email: newUser.email,
+        name: newUser.name,
+        role: newUser.role,
+        image: newUser.image,
+      },
+    });
+  } catch (error) {
+    console.log("error creating user: ", error);
     res.status(500).json({
-      error:"Error login user"
-  })
+      error: "Error creating user",
+    });
   }
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(401).json({
+        error: "User is not found",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        erro: "Invalid credentials",
+      });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
 
-}
-export const logout = async(req,res)=>{
-  try{
-  res.clearCookie("jwt",{
-    httpOnly:true,
-    sameSite:'strict',
-    secure:process.env.NODE_ENV !== "development",
-  })
-  res.status(200).json({
-    success:true,
-    message:"user logged out successfully"
-  })
-  }catch(error){
-            console.log('error logging out user:',error);
-            res.status(500).json({
-              error:"Error logging out user"
-            })
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        image: user.image,
+      },
+    });
+  } catch (error) {
+    console.log("Error Sign user:", error);
+    res.status(500).json({
+      error: "Error login user",
+    });
   }
-
-}
-export const check = async(req,res)=>{
-          try{
-            res.status(200).json({
-              success:true,
-              message:"user authenticate successfully",
-              user:req.user
-            })
-          }catch(error){
-            console.log('error in checking user ',error)
-            return res.status(404).json({
-                 error:"Error checking user"
-            })
-          }
-}
+};
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+    res.status(200).json({
+      success: true,
+      message: "user logged out successfully",
+    });
+  } catch (error) {
+    console.log("error logging out user:", error);
+    res.status(500).json({
+      error: "Error logging out user",
+    });
+  }
+};
+export const check = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "user authenticate successfully",
+      user: req.user,
+    });
+  } catch (error) {
+    console.log("error in checking user ", error);
+    return res.status(404).json({
+      error: "Error checking user",
+    });
+  }
+};
--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,61 +1,66 @@
-import {db} from '../libs/db.js';
-import { submitBatch,pollBatchResults } from '../libs/judge0.lib.js';
-export const  createProblem =async(req,res)=>{
-             const {title ,description,difficulty,tags,example,constraints,testcases,codeSnippets ,referenceSolution}= req.body;
+import { db } from "../libs/db.js";
+import { submitBatch, pollBatchResults } from "../libs/judge0.lib.js";
+export const createProblem = async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    example,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolution,
+  } = req.body;
 
-             if(req.user.role!=="ADMIN"){
-               return res.status(403).json({
-                              error:"You are not allowede to create problem "
-               })
-             }
-          try{
-           for(const[language,solutionCode] of Object.entries(referenceSolution)){
-               const languageId =  getJudge0LanguageId(language);
-               if(!languageId){
-                              return res.status(400).json({
-                                             error:`Language${language} is not supported`
-                              })
-               }
+  if (req.user.role !== "ADMIN") {
+    return res.status(403).json({
+      error: "You are not allowede to create problem ",
+    });
+  }
+  try {
+    for (const [language, solutionCode] of Object.entries(referenceSolution)) {
+      const languageId = getJudge0LanguageId(language);
+      if (!languageId) {
+        return res.status(400).json({
+          error: `Language${language} is not supported`,
+        });
+      }
 
-               const submission = testcases.map(({input,output})=>({
-                         source_code:solutionCode,
-                         language_id:languageId,
-                         stdin:input,                                  
-                         expected_output:output,
+      const submission = testcases.map(({ input, output }) => ({
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      }));
+    }
+    const submissionResults = await submitBatch(submissions);
+    const tokens = submissionResults.map((res) => res.token);
+    const results = await pollBatchResults(tokens);
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+      console.log("Result------", result);
+      if (result.status.id !== 3) {
+        return res.status(400).json({
+          error: `Testcase ${i + 1} failed for language ${language}`,
+        });
+      }
+    }
+  } catch (error) {}
+};
 
-               }))
-           }
-           const submissionResults = await submitBatch(submissions);
-           const tokens  = submissionResults.map((res)=>res.token)
-           const results = await pollBatchResults(tokens);
-           for(let i =0;i<results.length;i++){
-            const result = results[i];
-            console.log("Result------",result);
-            if(result.status.id !== 3){
-              return res.status(400).json({
-                error:`Testcase ${i+1} failed for language ${language}`,
-              })
-            }
-           }
-          }
-          catch(error){
+export const getAllProblems = async (req, res) => {
 
-          }
-}
+};
+export const getProblemById = async (req, res) => {
 
-export const getAllProblems = async(req,res)=>{
+};
+export const updateProblem = async (req, res) => {
 
-}
-export const getProblemById = async(req,res)=>{
+};
+export const deleteProblem = async (req, res) => {
 
-}
-export const updateProblem= async(req,res)=>{
-
-
-}
-export const deleteProblem = async(req,res)=>{
-
-}
-export const getAllProblemsSolvedByUser = async(req,res)=>{
-
-}
+};
+export const getAllProblemsSolvedByUser = async (req, res) => {
+  
+};
